41ef02862ca7c00560a9d1179cb3cf28e2fdc402
Усман <@DESKTOP-E07M5P8>
2024-02-22 00:00:28 +0700

    Add project files.

---
+++ OnePlayerGame.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BullsCows
+{
+    public class OnePlayerGame
+    {
+        private static Random rnd = new Random();
+        private static int steps = 0, bulls = 0, cows = 0;
+        private static char[] enigma;
+
+        private void Init(int n = 4)
+        {
+            char[] abc = "0123456789".ToCharArray();
+
+            for (int i = 0; i < 4; i++)
+            {
+                var j = rnd.Next(i + 1, 10);
+                (abc[i], abc[j]) = (abc[j], abc[i]);
+            }
+            enigma = abc[..4];
+        }
+
+        private void Loop()
+        {
+            do
+            {
+                steps++;
+                var hypothesis = Input(">>>");
+                if (string.IsNullOrEmpty(hypothesis))
+                    break;
+                if (Verificate(hypothesis, out var error))
+                {
+                    bulls = 0; cows = 0;
+                    for (int i = 0; i < 4; i++)
+                        if (hypothesis[i] == enigma[i])
+                            bulls++;
+                        else if (enigma.Contains(hypothesis[i]))
+                            cows++;
+                    if (bulls == 4)
+                        Console.WriteLine("You WIN!!!");
+                    else
+                        Print($"{steps}:  Bull:{bulls} Cows:{cows}");
+                }
+                else
+                {
+                    Print(error);
+                }
+
+            } while (bulls < 4);
+
+        }
+
+        private bool Verificate(string hypothesis, out string error)
+        {
+            error = "";
+            string errorMessage = "Гипотеза должна состоять из четырёх различных цифр";
+            if (hypothesis.Length != 4)
+                error = errorMessage;
+            else if (hypothesis.Distinct().Count() != 4)
+                error = errorMessage;
+            else if (hypothesis.Any(c => !char.IsDigit(c)))
+                error = errorMessage;
+            return error == "";
+        }
+
+        private string Input(string hint = "")
+        {
+            Console.Write(hint);
+            return Console.ReadLine();
+        }
+
+        private static void Print(string? str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public void Run()
+        {
+            Init();
+            Loop();
+            Print("Заходи ещё!");
+
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BullsCows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите режим игры (0 - игра с компьютером, 1 - игра с другом):");
+            int gameMode = int.Parse(Console.ReadLine());
+            var mode = GameMode.OnePlayer;
+            switch (gameMode)
+            {
+                case 0:
+                    mode = GameMode.OnePlayer;
+                    break;
+                case 1:
+                    mode = GameMode.TwoPlayers;
+                    break;
+                default:
+                    Console.WriteLine("Введите 0 или 1!");
+                    break;
+            }
+
+            var bc = new BullsCows(mode);
+
+        }
+    }
+}
